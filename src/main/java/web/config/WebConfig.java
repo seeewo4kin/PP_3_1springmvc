c972cc15489218766a3e0ea2203a9e76711f6a93
seeewo4kin <sewo4kin321@gmail.com>
2024-03-08 10:10:56 +0300

    comment

--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -46,5 +46,7 @@ public class WebConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+
     }
+
 }
